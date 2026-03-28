e7b43a4c0433d94e019199b968c0d17ebb5cf7f8
faisalkhan-fk <faisal.25161094@kiet.edu>
2026-03-29 02:22:22 +0530

    Implement lambda expression for square calculation

---
+++ Lambda-Expressions-Practice/Single-Parameter/Solution.java
@@ -0,0 +1,15 @@
+package advancejava_sec_B;
+
+public class lambda3 {
+
+    interface MathOperation {
+        int display(int x);
+    }
+
+    public static void main(String[] args) {
+
+        MathOperation m = (x) -> x * x;
+
+        System.out.println("Square = " + m.display(5));
+    }
+}
