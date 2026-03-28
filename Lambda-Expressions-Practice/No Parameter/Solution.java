b08bd552d99cd3c2bcc66ad17f212eb57e2a9389
faisalkhan-fk <faisal.25161094@kiet.edu>
2026-03-29 02:17:29 +0530

    No parameter

---
+++ Lambda-Expressions-Practice/No Parameter/Solution.java
@@ -0,0 +1,15 @@
+package advancejava_sec_B;
+
+public class lambda1 {
+
+    interface NoParameter {
+        String display();
+    }
+
+    public static void main(String[] args) {
+
+        NoParameter n2 = () -> "hello Lambda!";
+
+        System.out.println(n2.display());
+    }
+}
