04eda4bdf51857f463b0e88057c36e15b2dc6021
Faisal Khan <faisal.25161094@kiet.edu>
2026-03-31 02:28:42 +0530

    Add Java Database Connectivity

--- JDBC/Database-Connectivity.java
+++ JDBC/Database-Connectivity.java
@@ -1,1 +1,22 @@
+//Java Database Connectivity
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class DBconnect {
+
+	public static void main(String[] args) {
+		try {
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+			Connection con = DriverManager.getConnection(
+				"jdbc:oracle:thin:@localhost:1521:XE",
+				"System",
+				"Khan@2230"
+			);
+			System.out.println("Connected");
+		}
+		catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
