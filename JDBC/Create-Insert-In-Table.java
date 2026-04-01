4c41a6d8ae6948a0cb8ff44402cb131e7ef06b0f
Faisal Khan <faisal.25161094@kiet.edu>
2026-04-01 20:33:36 +0530

    Create table and data insertion
    Added code to create a connection to an Oracle database, execute an insert query, and handle exceptions.

--- JDBC/Create-Insert-In-Table.java
+++ JDBC/Create-Insert-In-Table.java
@@ -1,1 +1,42 @@
+//Create table and Insert data into the table in java
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+
+public class DBconnect {
+
+    public static void main(String[] args) {
+        try {
+            // Load Oracle Driver
+            Class.forName("oracle.jdbc.driver.OracleDriver");
+
+            // Create Connection
+            Connection con = DriverManager.getConnection(
+                "jdbc:oracle:thin:@localhost:1521:XE",
+                "System",
+                "Khan@2230"
+            );
+
+            System.out.println("Connected");
+
+            // Create Statement
+            Statement st = con.createStatement();
+
+            // SQL Query
+            String query = "Insert into student1 values('1','Faisal Khan')";
+
+            // Execute Query
+            st.executeUpdate(query);
+
+            System.out.println("Data Inserted");
+
+            // Close connection
+            st.close();
+            con.close();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
