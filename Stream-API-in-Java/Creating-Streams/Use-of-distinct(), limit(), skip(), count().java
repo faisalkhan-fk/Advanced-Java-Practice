9995088817bb60b50e7e82666c5b3dce1cf5323c
Faisal Khan <faisal.25161094@kiet.edu>
2026-04-03 10:45:55 +0530

    Implement examples of Stream API methods
    Demonstrate the use of distinct(), limit(), skip(), and count() methods in Java Stream API.

--- Stream-API-in-Java/Creating-Streams/Use-of-distinct(), limit(), skip(), count().java
+++ Stream-API-in-Java/Creating-Streams/Use-of-distinct(), limit(), skip(), count().java
@@ -1,1 +1,29 @@
+//Use of distinct(), limit(), skip(), count() in stream API
+
+package advancejava_sec_B;
+import java.util.List;
+public class streamapi3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		List<Integer> l1=List.of(10,20,30,45,56,10,6,34,30);
+		List<Integer> l2=l1.stream().distinct().toList();
+		List<Integer> l3=l1.stream().distinct().skip(3).toList();
+		List<Integer> l4=l1.stream().distinct().skip(1).limit(4).toList();
+		long c2=l1.stream().distinct().skip(1).limit(4).count();
+
+
+		System.out.println(l2);
+		System.out.println(l3);
+		System.out.println(l4);
+		long c=l1.stream().count();
+		System.out.println(c);
+		System.out.println(c2);
+
+		//l1.stream().distinct().forEach(System.out::println);
+		//l1.stream().distinct().forEach(System.out::println);
+
+		}
+
+}
 
