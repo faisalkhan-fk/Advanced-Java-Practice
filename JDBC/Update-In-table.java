261e2214c8691a1fbaa3018bfd641a28ff9c2b39
Faisal Khan <faisal.25161094@kiet.edu>
2026-04-06 18:26:56 +0530

    Add JDBC program to update student name in database
    This Java program connects to an Oracle database and updates a student's name in the 'student1' table.

---
+++ JDBC/Update-In-table.java
@@ -0,0 +1,37 @@
+//Update in table JDBC
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.Scanner;
+
+public class DBconnect {
+
+    public static void main(String[] args) {
+        try {
+            // Load Oracle Driver
+            Class.forName("oracle.jdbc.driver.OracleDriver");
+
+            // Create Connection
+            Connection con = DriverManager.getConnection(
+                "jdbc:oracle:thin:@localhost:1521:XE",
+                "System",
+                "Khan@2230"
+            );
+
+            System.out.println("Connected");
+
+            // Create Statement
+            Statement st = con.createStatement();
+           String query="Update student1 set name='Vaibhav' where id=2";
+            st.execute(query);
+            System.out.println("Data Updated");
+           st.close();
+            con.close();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
