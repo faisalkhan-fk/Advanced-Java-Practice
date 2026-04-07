0077d8513a4967a273b0b4fdb318402e24c27f54
Faisal Khan <faisal.25161094@kiet.edu>
2026-04-07 16:09:46 +0530

    Implement data deletion using PreparedStatement
    Added a prepared statement to delete a student record by ID.

---
+++ JDBC/Data-Deletion-by-prepareStatement.java
@@ -0,0 +1,34 @@
+// Data Deletion throught prepareStatement
+
+import java.sql.*;
+import java.util.Scanner;
+import java.util.*;
+
+public class preparest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		try {
+            // Load Oracle Driver
+            Class.forName("oracle.jdbc.driver.OracleDriver");
+
+            // Create Connection
+            Connection con = DriverManager.getConnection(
+                "jdbc:oracle:thin:@localhost:1521:XE",
+                "System",
+                "Khan@2230"
+            );
+
+            System.out.println("Connected");
+      String query="delete from student1 where id=?";
+              PreparedStatement pst=con.prepareStatement(query);
+              pst.setInt(1,2);
+              pst.executeUpdate();
+              System.out.println("Data Deleted successfully");
+       } catch (Exception e) {
+	            e.printStackTrace();
+	        }
+	    }
+	}
+	
+
