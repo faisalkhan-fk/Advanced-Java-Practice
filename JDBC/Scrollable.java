03a41fc868baf47034cff5ad9542754785d6a9f1
Faisal Khan <faisal.25161094@kiet.edu>
2026-04-08 12:32:17 +0530

    Add JDBC Scrollable ResultSet example
    Implement a Java program to scroll through a database result set using TYPE_SCROLL_INSENSITIVE.

---
+++ JDBC/Scrollable.java
@@ -0,0 +1,43 @@
+//Scroll the curcur backword and forward using TYPE_SCROLL_INSENSITIVE
+
+import java.sql.*;
+import java.util.Scanner;
+import java.util.*;
+
+public class scrollrs {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		try {
+            // Load Oracle Driver
+            Class.forName("oracle.jdbc.driver.OracleDriver");
+
+            // Create Connection
+            Connection con = DriverManager.getConnection(
+                "jdbc:oracle:thin:@localhost:1521:XE",
+                "System",
+                "Khan@2230"
+            );
+
+      System.out.println("Connected");
+      Statement st=con.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE,ResultSet.CONCUR_READ_ONLY);
+      ResultSet rs=st.executeQuery("Select * from student1");
+      
+      while(rs.next()) {
+    	  System.out.println(rs.getInt(1)+" "+rs.getString(2));
+      }
+      System.out.println("------backward-----");
+      rs.afterLast();
+      while(rs.previous()) {
+    	  System.out.println(rs.getInt(1)+" "+rs.getString(2));
+		}
+      //At specific position
+      rs.absolute(5);
+      System.out.println("name at 5th row "+rs.getString("name"));
+	}
+		
+      catch (Exception e) {
+          e.printStackTrace();
+      }
+  }
+}
