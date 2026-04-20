cf1d89c1b77cc9ab2eab8a830dd1b1324db2e343
Faisal Khan <faisal.25161094@kiet.edu>
2026-04-20 19:35:00 +0530

    Refactor import statements in min().java
    Updated import statements for clarity.

--- Stream-API-in-Java/Creating-Streams/min().java
+++ Stream-API-in-Java/Creating-Streams/min().java
@@ -1,7 +1,7 @@
 //min() stream me se minimum (sabse chhota element) return karta hai
 
 import java.util.*;
-import java.util.stream.*;
+
 public class Test {
     public static void main(String[] args) {
         List<Integer> l = List.of(10, 25, 5, 40, 15);
