1e644a27e402645c945066eba319b764e5d8b533
Faisal Khan <faisal.25161094@kiet.edu>
2026-04-20 19:36:37 +0530

    Clean up imports in reduce() example
    Removed unused import statements for stream functionality.

--- Stream-API-in-Java/Creating-Streams/reduce().java
+++ Stream-API-in-Java/Creating-Streams/reduce().java
@@ -1,8 +1,6 @@
 //reduce() function used in stream API
 
 import java.util.*;
-import java.util.stream.*;
-
 public class Test {
     public static void main(String[] args) {
         List<Integer> l = List.of(1,2,3,4,5);
