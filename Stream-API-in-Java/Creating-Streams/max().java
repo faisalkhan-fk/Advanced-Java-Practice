811545f748545fb442427f1fb0228128bccb77a4
Faisal Khan <faisal.25161094@kiet.edu>
2026-04-20 19:00:10 +0530

    Add max() method example in Stream API
    Added max() method example in Stream API.

---
+++ Stream-API-in-Java/Creating-Streams/max().java
@@ -0,0 +1,15 @@
+//max() stream me se maximum (sabse bada element) return karta hai
+
+import java.util.*;
+import java.util.stream.*;
+public class Test {
+    public static void main(String[] args) {
+        List<Integer> l = List.of(10, 25, 5, 40, 15);
+
+        int max = l.stream()
+                   .max((a,b) -> a - b)
+                   .get();
+
+        System.out.println(max);
+    }
+}
