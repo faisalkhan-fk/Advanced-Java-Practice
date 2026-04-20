6e5cf117982c3dd9c40c912b7c8d6acd1e634652
Faisal Khan <faisal.25161094@kiet.edu>
2026-04-20 18:54:14 +0530

    Add example of sorting a list using streams

---
+++ Stream-API-in-Java/Creating-Streams/sorted.java
@@ -0,0 +1,14 @@
+//sorted() -> sorted list in ascending order
+import java.util.*;
+import java.util.stream.*;
+
+
+public class Test {
+    public static void main(String[] args) {
+        List<Integer> l = List.of(5,2,8,1,3);
+
+        List<Integer> sortedList = l.stream().sorted().toList();
+
+        System.out.println(sortedList);
+    }
+}
